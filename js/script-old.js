06318a9c541ba4e7a1ae80efbb1196fa5198ad1c
Alex <chebotarov1989@gmail.com>
2022-03-16 17:05:44 +0200

    my

---
+++ js/script-old.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// alert('Hello');
+
+// const result = confirm("Are yoy here?");
+// console.log(result);
+
+// const answer = prompt("Are you 18 years?", "");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your surname?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Hello ${user}`);
+
+// console.log('arr' + ' - object');
+// console.log(4 + +'5');
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 == 8);
+
+// &&
+// ||
+// ! - отрицание
+
+// const isChecked = false,
+//         isClose = false;
+
+//         console.log(isChecked || !isClose);
+
+const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+const personalMovieDB {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = ptompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = ptompt('На сколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
